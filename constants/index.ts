88d6f01cefa3edc85e050bf648527b594fbff975
Kaung Zaw Thant <kaung.thant@aiesec.net>
2024-10-04 05:05:40 -0500

    Added const sidebarLinks

---
+++ constants/index.ts
@@ -0,0 +1,25 @@
+//This is the const data used in the website
+
+export const sidebarLinks = [
+    {
+        imgURL : '/icons/home.svg',
+        route : '/',
+        label: 'Home'
+    },
+    {
+        imgURL: '/icons/dollar-circle.svg',
+        route: '/my-banks',
+        label: 'My Banks'
+    },
+    {
+        imgURL: '/icons/transaction.svg',
+        route: '/transactions',
+        label: 'Transactions'
+    },
+    {
+        imgURL: '/icons/money-send.svg',
+        route: '/payment-transfer',
+        label: 'Transfer Funds'
+    },
+
+];
